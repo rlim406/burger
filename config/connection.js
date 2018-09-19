ca6d9d42295a5065d6848c566aab1497020d8db2
r.lim406 <r.lim406@hotmail.com>
2018-09-19 17:04:53 -0400

    fixed connection

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@ var mysql = require("mysql");
 
 var connection = mysql.createConnection({
   host: "localhost",
-  port: 3306,
+  port: 8000,
   user: "root",
   password: "password",
   database: "burgers_db"
