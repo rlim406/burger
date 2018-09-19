a462c238aab86a1299114a00b4575484132449a1
r.lim406 <r.lim406@hotmail.com>
2018-09-19 12:28:28 -0400

    created folders and files

---
+++ db/seeds.sql
@@ -0,0 +1,17 @@
+USE burgers_db;
+
+INSERT INTO burgers
+  (burger_name,devoured)
+VALUES
+  ("The Baconator", false);
+
+INSERT INTO burgers
+  (burger_name,devoured)
+VALUES
+  ("The Big Regret", false);
+
+INSERT INTO burgers
+  (burger_name,devoured)
+VALUES
+  ("The Lightweight", false);
+
